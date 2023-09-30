648d54efaa3c42e6c5b0d37d5739ab880814ffc9
Berke Sayın <sayinberkesayin@gmail.com>
2023-09-30 16:01:47 +0300

    Add test functions

---
+++ index.test.js
@@ -0,0 +1,23 @@
+const {
+  sum,
+  subtraction,
+  product,
+  division,
+  myBeverage,
+} = require("./index.js");
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test("subtracts 2 from 10 to equal 8", () => {
+  expect(subtraction(10, 3)).toBe(7);
+});
+
+test("multiplies 5 with 9 to equal 45", () => {
+  expect(product(5, 9)).toBe(45);
+});
+
+test("divides 20 to 2 to equal 10", () => {
+  expect(division(20, 2)).toBe(10);
+});
