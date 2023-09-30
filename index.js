73677274a3f4e3a75df31551060112c60bd3691e
Berke Sayın <sayinberkesayin@gmail.com>
2023-09-30 15:17:11 +0300

    Create mathematical functions and export them

---
+++ index.js
@@ -0,0 +1,22 @@
+const sum = (a, b) => {
+  return a + b;
+};
+
+const subtraction = (c, d) => {
+  return c - d;
+};
+
+const product = (e, f) => {
+  return e * f;
+};
+
+const division = (g, h) => {
+  return g / h;
+};
+
+module.exports = {
+  sum,
+  subtraction,
+  product,
+  division,
+};
